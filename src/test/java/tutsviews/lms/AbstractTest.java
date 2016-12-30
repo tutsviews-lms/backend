cca9473dd30f89405024a32b09d943053efcd21b
AladinZaier <zaier.alaeddine@gmail.com>
2016-12-30 16:33:13 +0100

    test activated with 2 test profiles in abstract test

--- src/test/java/tutsviews/lms/AbstractTest.java
+++ src/test/java/tutsviews/lms/AbstractTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -16,12 +17,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
+@ActiveProfiles(profiles={"test","local-test"})
+//@WebAppConfiguration needed due to swagger failures while running test. https://github.com/springfox/springfox/issues/654
 @WebAppConfiguration
 public abstract class AbstractTest {
 
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	
-	
-	
 }

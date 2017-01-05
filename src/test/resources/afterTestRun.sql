775738e0606544258b77c6a079201b41316e9f8a
AladinZaier <zaier.alaeddine@gmail.com>
2017-01-05 12:28:28 +0100

    sql commands added to afterTestRun.sql and beforeTestRun tested and validated

--- src/test/resources/afterTestRun.sql
+++ src/test/resources/afterTestRun.sql
@@ -1,1 +1,24 @@
 DELETE FROM personne;
+DELETE FROM content;
+
+DELETE FROM audio;
+DELETE FROM video;
+DELETE FROM text;
+
+DELETE FROM lecture;
+DELETE FROM Section;
+
+DELETE FROM course_category;
+DELETE FROM category;
+DELETE FROM Pricing;
+
+DELETE FROM course;
+DELETE FROM author;
+DELETE FROM image;
+
+
+
+
+
+
+

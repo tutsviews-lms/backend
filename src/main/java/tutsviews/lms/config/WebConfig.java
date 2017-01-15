a6546d24ce1b9c527d15d7cf54e4aaf148683987
AladinZaier <zaier.alaeddine@gmail.com>
2017-01-15 13:38:32 +0100

    validate a GlobalInterceptor with HitCounter Bean

--- src/main/java/tutsviews/lms/config/WebConfig.java
+++ src/main/java/tutsviews/lms/config/WebConfig.java
@@ -4,42 +4,57 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import tutsviews.lms.web.rest.data.HitCounter;
+import tutsviews.lms.web.rest.interceptors.GlobalInterceptor;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
+	@Autowired
+	private ApplicationContext applicationContext;
 	
-	  private ApplicationContext applicationContext;
+	 @Autowired
+	 GlobalInterceptor globalInterceptor;
+	 
+	 @Autowired 
+	 HitCounter hitCounter;
 
+	public WebConfig() {
+		super();
+	}
+	
+	
 
-	    public WebConfig() {
-	        super();
-	    }
-	    
-	    
-	    
 	@Override
-	  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(globalInterceptor);
+		super.addInterceptors(registry);
+	}
+
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	@Bean
+	@Scope("prototype")
+	public Logger logger(InjectionPoint injectionPoint) {
+		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+	}
+
 	
+
 	
-	
-	
-	 @Bean
-	    @Scope("prototype")
-	    public Logger logger(InjectionPoint injectionPoint) {
-	        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
-	    }
-	
-	
-	
-	
+	 
 }

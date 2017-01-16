47ec9fcb8ceff90dd3a544cb032f05e0bfd58eba
AladinZaier <zaier.alaeddine@gmail.com>
2017-01-16 16:15:03 +0100

    data changed in BeforeTesRun.sql

--- src/test/resources/beforeTestRun.sql
+++ src/test/resources/beforeTestRun.sql
@@ -26,8 +26,8 @@ INSERT INTO `video` VALUES ('2', '2017-01-05 10:58:42', '120', 'video 2', '/reso
 INSERT INTO `author` VALUES ('1', '2017-01-05 11:04:59', 'Montpellier', 'France', 'avenue du professeur louis ravas', '34080', 'le meilleur auteur du monde', 'zaier.alaeddine@gmail.com', 'Alaeddine', 'Zaier', 'coucou', '0652522113', '1');
 INSERT INTO `author` VALUES ('2', '2017-01-05 11:07:01', 'Fontainebleau', 'France', 'chemin des étoiles', '92000', 'auteur très geek', 'houssem.zaier@gmail.com', 'Houssem', 'Zaier', 'coucou', '0781490988', '2');
 INSERT INTO `author` VALUES ('3', '2017-01-05 11:07:01', 'Nabeul', 'Tunisie', 'impasse des lacs', '92000', 'auteur très connu en afrique', 'houssem.zaier@gmail.com', 'Houssem eddine', 'Zaier', 'coucou', '0781490988', '2');
-INSERT INTO `author` VALUES ('4', '2017-01-05 11:07:01', 'Nabeul', 'Tunisie', 'impasse des lacs', '92000', 'aucun livre édité', 'houssem.zaier@gmail.com', 'Refka', 'Zaier', 'coucou', '0781490988', null);
-INSERT INTO `author` VALUES ('5', '2017-01-05 11:07:01', 'Nabeul', 'Tunisie', 'impasse des lacs', '92000', 'auteur très connu en afrique', 'houssem.zaier@gmail.com', 'Duboit', 'F', 'coucou', '0781490988', null);
+INSERT INTO `author` VALUES ('4', '2017-01-05 11:07:01', 'Nabeul', 'Tunisia', 'impasse des lacs', '92000', 'aucun livre édité', 'houssem.zaier@gmail.com', 'Refka', 'Zaier', 'coucou', '0781490988', null);
+INSERT INTO `author` VALUES ('5', '2017-01-05 11:07:01', 'Nabeul', 'Tunisia', 'impasse des lacs', '92000', 'auteur très connu en afrique', 'houssem.zaier@gmail.com', 'Duboit', 'F', 'coucou', '0781490988', null);
 INSERT INTO `author` VALUES ('8', '2017-01-12 10:57:58', null, null, null, null, 'f5a5ri', 'zaier.mohsen@gmail.com', 'mohsen', 'zaier', '0505', '0652', null);
 INSERT INTO `author` VALUES ('9', '2017-01-12 11:07:22', null, null, null, null, 'kalb', 'azz.ff@gmail.com', 'aaa', 'aloulou', 'zeze', '065252', null);
 INSERT INTO `author` VALUES ('10', '2017-01-13 00:46:59', '', '', '', '', 'Raaaafka', 'azz.ff@gmail.com', 'Rafka', 'Refka', 'zeze', '065252', null);

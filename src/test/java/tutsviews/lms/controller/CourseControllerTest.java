5dd4e4abbd89980aad5b647e5f3facb2068762dd
AladinZaier <zaier.alaeddine@gmail.com>
2017-01-25 15:47:00 +0100

    HomeCOntrollerTest implemented and home() method tested

--- src/test/java/tutsviews/lms/controller/CourseControllerTest.java
+++ src/test/java/tutsviews/lms/controller/CourseControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import tutsviews.lms.AbstractTest;
